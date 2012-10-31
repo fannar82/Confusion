48c1bc9797a1822c09effb527bf02faa66d60c06
jonhallur <jonhallur@gmail.com>
2012-10-31 21:15:15 +0000

    added jonhallur to the main program and makefile

--- HelloWorld/hello.cpp
+++ HelloWorld/hello.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "jonhallur.h"
 using namespace std;
 
 int main()
 {
+    jonhallur jonh;
     cout<< "Hello world\nHello guys\n";
-
+    cout << jonh << endl;
     return 0;
 }
