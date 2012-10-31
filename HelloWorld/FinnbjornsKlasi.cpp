9f5bd8d2a7cb7f8ea7a066362f870f6205b6f2e1
Finnbjörn <finnbjorn@HUGB-Linux1.(none)>
2012-10-31 19:52:59 +0000

    Baetti vid "FinnbjornsKlasi.h og .cpp"

---
+++ HelloWorld/FinnbjornsKlasi.cpp
@@ -0,0 +1,19 @@
+#include "FinnbjornsKlasi.h"
+
+String nafnid = "Finnbjorn";
+
+FinnbjornsKlasi::FinnbjornsKlasi()
+{
+    // deliberatly empty    
+}
+
+FinnbjornsKlasi::~FinnbjornsKlasi()
+{
+    // deliberatly empty
+}
+
+string getName ()
+{
+    return nafnid;
+}
+
---
+++ HelloWorld/FinnbjornsKlasi.h
@@ -0,0 +1,15 @@
+#ifndef FINNBJORNSKLASI_H
+#define FINNBJORNSKLASI_H
+
+
+class FinnbjornsKlasi
+{
+    public:
+        FinnbjornsKlasi();
+        virtual ~FinnbjornsKlasi();
+        string getName();
+    protected:
+    private:
+};
+
+#endif // FINNBJORNSKLASI_H
