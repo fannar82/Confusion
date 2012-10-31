911ff93f6222073d792bf55a64c61dfb6f414c43
Finnbjörn <finnbjorn@HUGB-Linux1.(none)>
2012-10-31 20:54:47 +0000

    Finnbjorns klasa punktur h baett vid

--- HelloWorld/FinnbjornsKlasi.h
+++ HelloWorld/FinnbjornsKlasi.h
@@ -13,3 +13,20 @@ class FinnbjornsKlasi
 };
 
 #endif // FINNBJORNSKLASI_H
+#ifndef FINNBJORNSKLASI_H
+#define FINNBJORNSKLASI_H
+#include <iostream>
+#include <string>
+
+class FinnbjornsKlasi
+{
+    public:
+        /*FinnbjornsKlasi();
+        virtual ~FinnbjornsKlasi();*/
+        std::string getName() { return "FinnbjornsKlasi, gaman gaman."; };
+    protected:
+    private:
+};
+
+#endif // FINNBJORNSKLASI_H
+
