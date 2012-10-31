7aa2453a35e6203a1176ee08637e25d021ba3aa8
Finnbjörn <finnbjorn@HUGB-Linux1.(none)>
2012-10-31 21:12:01 +0000

    Finnbjorn breytti hello.cpp

--- HelloWorld/hello.cpp
+++ HelloWorld/hello.cpp
@@ -5,5 +5,11 @@ int main()
 {
     cout<< "Hello world\nHello guys\n";
 
+    // Fra Finnbirni
+    FinnbjornsKlasi fk;
+
+    string stuff = fk.getName();
+    // buid fra Finnbirni
+
     return 0;
 }
