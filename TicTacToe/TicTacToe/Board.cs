a37d53f1f1b665e2d77f9ee34303d865ca6a8b18
unknown <finnbjorn@finnbjorn.com>
2012-11-14 23:05:39 +0000

    Breytti Board

--- TicTacToe/TicTacToe/Board.cs
+++ TicTacToe/TicTacToe/Board.cs
@@ -5,7 +5,20 @@ using System.Text;
 
 namespace TicTacToe
 {
-    class Board
+    public class Board
     {
+        private char[] board;
+
+        // Default constructor is of size 3
+        Board()
+        {
+            board = char[3,3]; 
+        }
+
+        // Custom size
+        Board(int size)
+        {
+            board = char[size,size]; 
+        }
     }
 }
