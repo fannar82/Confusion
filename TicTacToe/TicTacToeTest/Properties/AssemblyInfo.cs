d360e307a254f6580470d0c3301e276d90a54a3b
Fannar Már Sigurðsson <fannar82@gmail.com>
2012-11-14 22:44:20 +0000

    baeta vid testi

---
+++ TicTacToe/TicTacToeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyInfo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyInfo")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f5182d7-584d-4d1f-b5f0-eca926edb566")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TicTacToe/TicTacToeTest/TestFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TicTacToe;
+
+namespace TicTacToeTest
+{
+    [TestFixture]
+    public class TestFixture1
+    {
+
+        // This test fail for example, replace result or delete this test to see all tests pass
+        [Test]
+        public void TestFault()
+        {
+            FannarClass fannar = new FannarClass();
+
+            Assert.IsFalse(fannar.fannar());
+           
+        }
+    }
+}
