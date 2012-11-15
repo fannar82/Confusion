a026076b922c0a505b13773051e9a7db2a7c0fd3
Sigurður Jónsson <sigurdurjon11@ru.is>
2012-11-15 23:50:48 +0000

    BoardTestFixture.cs

---
+++ TicTacToe/TicTacToeTest/BoardTestFixture.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TicTacToe;
+using NUnit.Framework;
+
+namespace TicTacToeTest
+{
+    [TestFixture]
+    public class BoardTestFixture
+    {
+
+
+        [Test]
+        public void TestingArray_SendingCoord_OutofRange_ToLow()
+        {
+            //Arrange
+            Board br = new Board();
+            var expected = false;
+
+            //Act
+            var actioal = br.newMove(0, 0, "X");
+
+            //Assert
+            Assert.IsTrue(actioal.Equals(expected));
+
+        }
+
+        [Test]
+        public void TestingArrey_SendigCoord_OutofRange_ToHigh()
+        {
+            //Arrange
+            Board br = new Board();
+            var expected = false;
+
+            //Act
+            var actioal = br.newMove(5, 5, "X");
+
+            //Assert
+            Assert.IsTrue(actioal.Equals(expected));
+
+        }
+
+    }
+}
