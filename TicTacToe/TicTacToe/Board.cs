8264fe604b9d149c2077de3863171293e9ef281b
unknown <finnbjorn@finnbjorn.com>
2012-11-15 00:36:22 +0000

    Board uppfaert

--- TicTacToe/TicTacToe/Board.cs
+++ TicTacToe/TicTacToe/Board.cs
@@ -7,32 +7,46 @@ namespace TicTacToe
 {
     public class Board
     {
-        private char[,] board;
+        private string[,] board;
+        private int size;
 
         // Default constructor is of size 3
         public Board()
         {
-            board = new char[3,3]; 
+            board = new string[3,3]; 
         }
 
         // Custom size
         public Board(int size)
         {
-            board = new char[size,size]; 
+            board = new string[size, size];
+            //clearBoard();
         }
+
+        /*private void clearBoard()
+        {
+            foreach (string coord in board)
+            {
+                coord = null;
+            }
+        }*/
 
         /// <summary>
         /// Return true if the move was legal
         /// </summary>
         /// <param name="x">x coordination</param>
         /// <param name="y">y coordination</param>
+        /// <param name="value">the value that should be added</param>
         /// <returns></returns>
-        public bool newMove(int x, int y, char value)
+        public bool newMove(int x, int y, string value)
         {
-            if (board[x, y] != null)
+            if (x >= size || y >= size)
                 return false;
 
-            board[x, y] = value;
+            if (board[x-1, y-1] != null)
+                return false;
+
+            board[x-1, y-1] = value;
             return true;
         }
     }
