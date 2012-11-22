fd3480acf7af7b735cdb7b6df8b70a67d6cf039f
Hjalti Erdmann Sveinsson <erdmann123@gmail.com>
2012-11-22 15:13:20 +0000

    Hjalti inserting GamLogicTest.cs a little programming has taking place

---
+++ TicTacToe/TicTacToeTest/GameLogicTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TicTacToe;
+using NUnit.Framework;
+
+namespace TicTacToeTest
+{
+    [TestFixture]
+    class GameLogicTest
+    {
+        [Test]
+        public void TestingCreateGame_Player()
+        {
+            //Arrange
+            GameLogic GLnewgame = new GameLogic();
+            GLnewgame.createGame();
+                       
+            //Act
+            Player ActualPlayer1, ActualPlayer2;
+            ActualPlayer1 = GLnewgame.player1;
+            ActualPlayer2 = GLnewgame.player2;
+
+            //Assert
+            Assert.IsInstanceOf<Player>(ActualPlayer1 );
+            Assert.IsInstanceOf<Player>(ActualPlayer2);
+        }
+
+        [Test]
+        public void TestingCreateGame_Board()
+        {
+            //Arrange
+            GameLogic GLnewgame = new GameLogic();
+            GLnewgame.createGame();
+
+            //Act
+            Board ActualBoard;
+            ActualBoard = GLnewgame.gameBoard;
+            
+            //Assert
+            Assert.IsInstanceOf<Board>(ActualBoard);
+
+        }
+
+        }
+}
