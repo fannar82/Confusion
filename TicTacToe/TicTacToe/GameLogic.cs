c2d89eec38721450bd27af022fc491ce23781388
Hjalti Erdmann Sveinsson <erdmann123@gmail.com>
2012-11-22 16:15:35 +0000

    Hjalti has made more codeing

--- TicTacToe/TicTacToe/GameLogic.cs
+++ TicTacToe/TicTacToe/GameLogic.cs
@@ -7,7 +7,8 @@ namespace TicTacToe
 {
     public class GameLogic
     {
-        private int moveCount, gameCount;
+  // ath breyti moveCount og gameCount í public til að geta gert fleiri próf (sleppti því að búa til get og set á þær).
+        public int moveCount, gameCount;
         public Player player1;
         public Player player2;
         public Board gameBoard;
@@ -18,21 +19,73 @@ namespace TicTacToe
         /// </summary>
         public void startGame()
         {
-            createGame();
+            createPlayers();
+            playNewGame();
+            
 
         }
-
 
 
         /// <summary>
-        /// Creates two players and a board all you need for a fun game.
+        /// This function is called to play a new game aftar createPlayers has been called.
         /// </summary>
-        public void createGame ()
+        public void playNewGame()
+        {
+            newBoard();
+//            if (desidePlayersTurn() == 1)
+
+        
+        }
+
+        /// <sumary>
+        /// This funciton desieds witch players turn it is.
+        /// </sumary>
+        public int desidePlayersTurn()
+        {
+            if (moveCount % 2 == 0)
+            { return 1; }
+            else
+            { return 2; }
+        }
+
+        /// <summary>
+        /// Creates two players and and sets gameCounts to zero.
+        /// </summary>
+        public void createPlayers ()
         {
             player1 = new Player();
             player2 = new Player();
+            gameCount = 0;
+            
+        }
+
+        /// <summary>
+        /// Creates a new gameBoard and sets moveCount to zero.
+        /// </summary>
+        public void newBoard()
+        {
             gameBoard = new Board();
+            moveCount = 0;
 
         }
+
+
+        /// <summary>   
+        /// Returns the value of moveCount
+        /// </summary>
+        public int getMoveCount()
+        {
+            return moveCount;
+        }
+
+        /// <summary>   
+        /// Returns the value of gameCount
+        /// </summary>
+        public int getGameCount()
+        {
+            return gameCount;
+        }
+
+
     }
 }
