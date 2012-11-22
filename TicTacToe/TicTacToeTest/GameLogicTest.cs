2caca6a72d77b679bcd676ef374c1adbb3a9db44
Hjalti Erdmann Sveinsson <erdmann123@gmail.com>
2012-11-22 16:16:41 +0000

    Hjalti has made more code

--- TicTacToe/TicTacToeTest/GameLogicTest.cs
+++ TicTacToe/TicTacToeTest/GameLogicTest.cs
@@ -11,11 +11,11 @@ namespace TicTacToeTest
     class GameLogicTest
     {
         [Test]
-        public void TestingCreateGame_Player()
+        public void TestingCreatePlayers()
         {
             //Arrange
             GameLogic GLnewgame = new GameLogic();
-            GLnewgame.createGame();
+            GLnewgame.createPlayers();
                        
             //Act
             Player ActualPlayer1, ActualPlayer2;
@@ -28,11 +28,11 @@ namespace TicTacToeTest
         }
 
         [Test]
-        public void TestingCreateGame_Board()
+        public void TestingNewBoard()
         {
             //Arrange
             GameLogic GLnewgame = new GameLogic();
-            GLnewgame.createGame();
+            GLnewgame.newBoard();
 
             //Act
             Board ActualBoard;
@@ -42,6 +42,65 @@ namespace TicTacToeTest
             Assert.IsInstanceOf<Board>(ActualBoard);
 
         }
+
+        [Test]
+        public void TestingnewBoard_moveCountToZero()
+        {
+            //Arrange
+            GameLogic GLnewgame = new GameLogic();
+            GLnewgame.createPlayers();
+            int expectedMoveCount = 0;
+
+
+            //Act
+            int actualMoveCount;
+            actualMoveCount = GLnewgame.getMoveCount();
+
+            //Assert
+            Assert.AreEqual (expectedMoveCount, actualMoveCount);
+
+        }
+
+        [Test]
+        public void TestingCreatePlayers_GameCountToZero()
+        {
+            //Arrange
+            GameLogic GLnewgame = new GameLogic();
+            GLnewgame.createPlayers();
+            int expectedGameCount = 0;
+
+            //Act
+            int actualGameCount;
+            actualGameCount = GLnewgame.getGameCount();
+
+            //Assert
+            Assert.AreEqual(expectedGameCount, actualGameCount);
+        }
+// svolítið asnalegt próf þar sem við viljum hafa moveCount private en verðum þá að setja get og set fyrir hana til að geta testað 
+// og hver er þá tilgangurinn að vera með hana private, ATH breytti henni í public.
+        [Test]
+        public void TestingDesedePlayersTurn()
+        {
+         //Arrange - test 0
+            GameLogic GLnewgame = new GameLogic();
+            GLnewgame.moveCount = 0;
+            int expectedReturn = 1;
+          //Act
+            int actualReturn;
+            actualReturn = GLnewgame.desidePlayersTurn();
+           //Assert
+            Assert.AreEqual(expectedReturn, actualReturn);
+        //Arrange - test 5
+            GLnewgame.moveCount = 5;
+            expectedReturn = 2;
+          //Act
+            actualReturn = GLnewgame.desidePlayersTurn();
+          //Assert
+            Assert.AreEqual(expectedReturn, actualReturn);
+
+        }
+
+
 
         }
 }
