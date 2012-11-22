b70983206dcc8fafaff4733d9d3cf10ef446429a
Hjalti Erdmann Sveinsson <erdmann123@gmail.com>
2012-11-22 15:07:40 +0000

    Hjalti inserting GameLogic - a little TDD programing has taking place

--- TicTacToe/TicTacToe/GameLogic.cs
+++ TicTacToe/TicTacToe/GameLogic.cs
@@ -5,7 +5,34 @@ using System.Text;
 
 namespace TicTacToe
 {
-    class GameLogic
+    public class GameLogic
     {
+        private int moveCount, gameCount;
+        public Player player1;
+        public Player player2;
+        public Board gameBoard;
+
+
+        /// <summary>
+        /// You have to call this function to start the game, have fun :)
+        /// </summary>
+        public void startGame()
+        {
+            createGame();
+
+        }
+
+
+
+        /// <summary>
+        /// Creates two players and a board all you need for a fun game.
+        /// </summary>
+        public void createGame ()
+        {
+            player1 = new Player();
+            player2 = new Player();
+            gameBoard = new Board();
+
+        }
     }
 }
