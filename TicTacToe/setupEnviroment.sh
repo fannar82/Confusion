16a886e1ff27ef1da61480d4a0f92578a3a4e5bc
jonhallur <jonhallur@gmail.com>
2012-11-26 02:00:00 +0000

    adding the setupEnviroment script

---
+++ TicTacToe/setupEnviroment.sh
@@ -0,0 +1,16 @@
+sudo apt-get install -y nant
+
+sudo apt-get install -y monodoc-base
+
+sudo apt-get install -y gendarme
+
+sudo apt-get install -y apache2
+
+sudo chmod 777 /var/www/
+sudo chmod 777 /var/www/index.html
+
+cd Confusion/TicTacToe
+sudo chmod 600 confusion-openssh
+
+./setupCron.sh
+
